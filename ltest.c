b7d0f51d7fdb5969bac8224ce4e354603609dc31
Zachary Wong <zachary.j.wong.23@dartmouth.edu>
2021-10-03 22:40:44 -0400

    another test push by Zach

--- ltest.c
+++ ltest.c
@@ -5,7 +5,7 @@
 /* /bin/Module3/ltest.c
  * first test of list implimentation by Zach Wong trying edits Mik
 >>>>>>> d3e401b5704a036c019c39058cc4a8675ff00cc9
- *
+ * Change to push 10:40pm
  */
 
 #include <stdio.h>
